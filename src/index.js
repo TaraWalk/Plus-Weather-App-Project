a2499a090f59f89ccee2f9d42f8b536bbf4e2903
TaraWalk <tarawalker1310@gmail.com>
2024-05-02 12:54:05 +0100

    Updated JS

--- src/index.js
+++ src/index.js
@@ -52,7 +52,34 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+       <div class="weather-forecast">
+   <div class="weather-forecast-day">
+              <div class="weather-forecast-date">${day}</div>
+              <br />
+              <div class="weather-forecast-icon">☼</div>
+              <div class="weather-forecast-temperature">
+                <br />
+                <div class="weather-forecast-temperature-maxi"> 18°</div>
+                <div class="weather-forecast-temperature-min"> 12°</div>
+        </div>
+`;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Tokyo");
+
+displayForecast();
