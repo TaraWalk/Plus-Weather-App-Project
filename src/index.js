aed1f2adc72b442a3297646ca435b815b490dc0e
TaraWalk <tarawalker1310@gmail.com>
2024-05-03 14:22:57 +0100

    Edited JS (API)

--- src/index.js
+++ src/index.js
@@ -53,6 +53,13 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
 function getForecast(city) {
   let apiKey = "62fb8e28a4eab4a583ea42bf3ofat058";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
@@ -60,18 +67,20 @@ function getForecast(city) {
 }
 
 function displayForecast(response) {
-  //console.log(response.data);
   let forecastHtml = "";
 
-  response.data.daily.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
       <div class="weather-forecast-day">
       <br  />
-        <div class="weather-forecast-date">${day}</div>
+        <div class="weather-forecast-date">${formatDay(day.time)}</div>
         <br />
-        <div class="weather-forecast-icon">☼</div>
+        <div class="weather-forecast-icon">
+        <img src="${day.condition.icon_url}" />
+        </div>
         <div class="weather-forecast-temperature">
           <br />
           <div class="weather-forecast-temperature-maxi">${Math.round(
@@ -83,6 +92,7 @@ function displayForecast(response) {
         </div>
       </div>
 `;
+    }
   });
 
   let forecastElement = document.querySelector("#forecast");
